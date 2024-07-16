e3b7320d0ea8370b6adfa7f83aef180a9abe9372
arthurJoly <arthur.joly@glovoapp.com>
2024-07-16 11:31:40 +0200

    refactor ecs construct

--- lib/constructs/ApiECS.ts
+++ lib/constructs/ApiECS.ts
@@ -1,45 +1,85 @@
-import {Construct} from 'constructs';
-import {aws_ec2 as ec2, aws_ecs as ecs} from 'aws-cdk-lib';
-import {LogDriver, Protocol} from "aws-cdk-lib/aws-ecs";
-import {LogGroup, RetentionDays} from "aws-cdk-lib/aws-logs";
+import { Construct } from 'constructs';
+import { Cluster, FargateService, FargateTaskDefinition, LogDriver, Protocol, RepositoryImage } from "aws-cdk-lib/aws-ecs";
+import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
+import { InstanceType, Vpc } from "aws-cdk-lib/aws-ec2";
+import {ApplicationListener, ApplicationLoadBalancer, ApplicationTargetGroup, IApplicationLoadBalancerTarget } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { HttpApi, HttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
+import { HttpAlbIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 
 export interface CustomProps {
-    // List all the properties
     name: string;
-    vpc: ec2.Vpc;
+    repositoryImage: RepositoryImage;
 }
+
 export class ApiECS extends Construct {
 
-    public ecsCluster : ecs.Cluster;
+    public ecsCluster : Cluster;
+    public ecsService : FargateService;
+    public fargateTaskDefinition: FargateTaskDefinition;
 
-    public ecsService : ecs.FargateService;
+    public vpc: Vpc;
+    public apiGateway: HttpApi;
 
-    // READ : https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs-readme.html
+    private applicationListener: ApplicationListener;
+
     constructor(scope: Construct, id: string, props: CustomProps) {
         super(scope, id);
-        this.ecsCluster = new ecs.Cluster(this, 'Cluster', { vpc: props.vpc });
-        this.ecsCluster.addCapacity('DefaultAutoScalingGroupCapacity', {
-            instanceType: new ec2.InstanceType("t2.xlarge"),
+       
+        this.createVpc(props.name)
+        this.createEcsCluster(props.name, props.repositoryImage);
+        this.createLoadBalancer(props.name, [this.ecsService])
+        this.createApiGateway(props.name, this.applicationListener)
+    }
+
+    private createVpc(name: string){
+        this.vpc = new Vpc(this, `vpc_${name}`, {
+            //TODO: why do we need to put false?
+            restrictDefaultSecurityGroup: false
+        });
+    }
+
+    private createEcsCluster(name: string, repositoryImage: RepositoryImage){
+        //TODO: give possibility to reuse cluster for different API
+        this.ecsCluster = new Cluster(this, `ecs_cluster_${name}`, { vpc: this.vpc });
+        this.ecsCluster.addCapacity(`scaling_group_capacity_${name}`, {
+            instanceType: new InstanceType("t2.xlarge"),
             desiredCapacity: 3,
         });
 
+        this.fargateTaskDefinition = new FargateTaskDefinition(this, `fargate_task_${name}`);
 
-        const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef');
-
-        taskDefinition.addContainer('DefaultContainer', {
-            //TODO: change to fromEcrRepository and pass the registry as props
-            image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+        this.fargateTaskDefinition.addContainer(`container_${name}`, {
+            image: repositoryImage,
             memoryLimitMiB: 512,
             portMappings: [{containerPort: 80, protocol: Protocol.TCP}],
-            logging: LogDriver.awsLogs({streamPrefix: "task", logGroup: new LogGroup(this, "taskLogGroup", {retention: RetentionDays.ONE_DAY})})
+            logging: LogDriver.awsLogs({streamPrefix: `container_${name}`, logGroup: new LogGroup(this, `log_${name}`, {retention: RetentionDays.ONE_DAY})})
         });
 
-
-        this.ecsService = new ecs.FargateService(this, 'Service', {
+        this.ecsService = new FargateService(this, `ecs_service_${name}`, {
             cluster : this.ecsCluster,
-            taskDefinition,
+            taskDefinition: this.fargateTaskDefinition
         });
+    }
 
+    private createLoadBalancer(name: string, targets: IApplicationLoadBalancerTarget[]){
+        var alb = new ApplicationLoadBalancer(this, `alb_${name}`, {
+            vpc: this.vpc,
+        });
+        var targetGroup = new ApplicationTargetGroup(this, `alb_target_group_${name}`, {port: 80, vpc: this.vpc})
+        this.applicationListener = alb.addListener(`alb_listener_${name}`, {port: 80, defaultTargetGroups: [targetGroup]})
+        this.applicationListener.addTargets(`default_target_${name}`, {port: 80, targets})
+    }
+
+    private createApiGateway(name: string, listener: ApplicationListener){
+        this.apiGateway = new HttpApi(this, `api_${name}`);
+        var link = this.apiGateway.addVpcLink({vpcLinkName: `api_link_${name}`, vpc: this.vpc })
+        var integration = new HttpAlbIntegration(`api_integration_${name}`, listener, {vpcLink: link} );
+    
+        this.apiGateway.addRoutes({
+            path: '/{proxy+}',
+            methods: [ HttpMethod.ANY ],
+            integration: integration
+        });
     }
 
     public grantDBWrite() {
--- lib/workfully-stack.ts
+++ lib/workfully-stack.ts
@@ -1,47 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ApiECS } from "./constructs/ApiECS";
-import { SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
-import {ApplicationLoadBalancer, ApplicationTargetGroup } from "aws-cdk-lib/aws-elasticloadbalancingv2";
-import { HttpApi, HttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
-import { HttpAlbIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { ContainerImage } from 'aws-cdk-lib/aws-ecs';
 
 export class WorkfullyStack extends cdk.Stack {
 
-  // https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-private-integration.html#http-api-private-integration-vpc-link
-  // https://github.com/Workfully-github/codechallenge-platform
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    var vpc = new Vpc(this, "myVpc", {
-      restrictDefaultSecurityGroup: false
-    });
-    
-    //TODO: add target group and listeners and rules
-    var alb = new ApplicationLoadBalancer(this, 'ALB', {
-      vpc,
-    });
-    var targetGroup = new ApplicationTargetGroup(this, "hello", {port: 80, vpc: vpc})
-    var listener = alb.addListener("default", {port: 80, defaultTargetGroups: [targetGroup]})
-    
-
-    var apiGateway = new HttpApi(this, "apiName");
-
-    var link = apiGateway.addVpcLink({vpcLinkName:"apiLink", vpc: vpc })
-
-    var integration = new HttpAlbIntegration("integration", listener, {vpcLink: link} );
-
-    apiGateway.addRoutes({
-      path: '/{proxy+}',
-      methods: [ HttpMethod.ANY ],
-      integration: integration,
-    });
-
-    var ecs = new ApiECS(this, "testECS", {
-      name: "test",
-      vpc: vpc
+    var apiEcs = new ApiECS(this, "WorkfullyEcsApi", {
+      name: "workfully",
+      repositoryImage: ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
     })
-
-    listener.addTargets("test", {port: 80, targets : [ecs.ecsService]})
   }
 }
